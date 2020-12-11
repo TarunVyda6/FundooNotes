5358ffc9851c24731f0914b7ac75438f352a2972
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-12-11 11:55:44 +0530

    Added : Feature of email verification during registration

---
+++ fundoo/fundoo/urls.py
@@ -0,0 +1,39 @@
+"""fundoo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from fundooapp import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginAPIView.as_view()),
+    path('register/', views.RegisterView.as_view()),
+    path('email-verify/', views.VerifyEmail.as_view(), name="email-verify"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('request-reset-email/', views.RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
+
+]
