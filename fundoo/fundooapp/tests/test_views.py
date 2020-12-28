87540aa07a305666b7e76639576d0753ca9990c9
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-12-28 11:53:52 +0530

    [Tarun] Refactored : Code to achieve DRY principle for pytest

--- fundoo/fundooapp/tests/test_views.py
+++ fundoo/fundooapp/tests/test_views.py
@@ -11,69 +11,70 @@ User = get_user_model()
 
 
 @pytest.mark.django_db
-class TestViews:
+class Data(TestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.login_url = reverse("login")
+        self.valid_registration_data = {'first_name': "tarun",
+                                        'last_name': "vyda",
+                                        'email': "kamaltarun.rao0@gmail.com",
+                                        'user_name': "tarunvyda",
+                                        'password': "adminpass"}
+        self.invalid_registration_data = {'first_name': "abc",
+                                          'last_name': "def",
+                                          'email': "abc123@gmail.com",
+                                          'user_name': "abcdef"}
+        self.valid_login_data = {
+            'email': "kamaltarun.rao0@gmail.com",
+            'password': "adminpass"}
+        self.invalid_login_data = {
+            'email': "kamaltarun.rao0@gmail.com",
+            'password': "adminpa"}
 
-    def test_valid_user_details_for_registration(self, client):
+
+class RegistrationTests(Data):
+
+    def test_given_valid_details_for_registration(self):
         """
         Ensure we can create a new account object.
         """
-        path = reverse('register')
-        valid_registration_data = {'first_name': "tarun",
-                                   'last_name': "vyda",
-                                   'email': "kamaltarun.rao0@gmail.com",
-                                   'user_name': "tarunvyda",
-                                   'password': "adminpass"}
-        response = client.post(path, data=valid_registration_data, format='json')
-        assert response.status_code == 201
 
-    def test_invalid_user_details_for_registration(self, client):
-        """
-        Ensure we cannot create a new account object with invalid credentials
-        """
-        invalid_registration_data = {'first_name': "abc",
-                                     'last_name': "def",
-                                     'email': "abc123@gmail.com",
-                                     'user_name': "abcdef"}
+        response = self.client.post(self.register_url, self.valid_registration_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = client.post(reverse('register'), invalid_registration_data, format='json')
-        assert response.status_code == 400
-
-    def test_valid_user_for_login(self, client):
+    def test_given_invalid_details_for_registration(self):
         """
-        Ensure we can login with valid credentials.
+        Ensure we cannot create a new account object.
         """
-        valid_registration_data = {'first_name': "tarun",
-                                   'last_name': "vyda",
-                                   'email': "kamaltarun.rao0@gmail.com",
-                                   'user_name': "tarunvyda",
-                                   'password': "adminpass"}
-        valid_login_data = {
-            'email': "kamaltarun.rao0@gmail.com",
-            'password': "adminpass"}
 
-        client.post(reverse('register'), valid_registration_data, format='json')
-        user = User.objects.filter(email=valid_registration_data['email']).first()
-        user.is_active = True
-        user.save()
-        response = client.post(reverse('login'), valid_login_data, format='json')
-        assert response.status_code == 200
+        response = self.client.post(self.register_url, self.invalid_registration_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_given_invalid_credentials_for_login(self, client):
+
+class LoginTest(Data):
+
+    def test_given_valid_credentials_login(self):
         """
-        Ensure we cannot login with invalid credentials.
+        Ensure we can login.
         """
-        valid_registration_data = {'first_name': "tarun",
-                                   'last_name': "vyda",
-                                   'email': "kamaltarun.rao0@gmail.com",
-                                   'user_name': "tarunvyda",
-                                   'password': "adminpass"}
-        invalid_login_data = {'email': "kamaltarun.rao0@gmail.com",
-                              'password': "adminpa"}
 
-        client.post(reverse('register'), valid_registration_data, format='json')
-        user = User.objects.filter(email=valid_registration_data['email']).first()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
         user.is_active = True
         user.save()
 
-        response = client.post(reverse('login'), invalid_login_data, format='json')
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_given_invalid_credentials_for_login(self):
+        """
+        Ensure we cannot login.
+        """
+
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_active = True
+        user.save()
+
+        response = self.client.post(self.login_url, self.invalid_login_data, format='json')
         assert response.status_code == 400
